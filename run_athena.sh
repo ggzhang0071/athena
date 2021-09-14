57d5a0550e73d868d775b9f99d2b1f75c9ec675e
93 machine <155051843@qq.com>
2021-09-14 10:25:15 +0800

    first commit

--- run_athena.sh
+++ run_athena.sh
@@ -1,2 +1,13 @@
-mkdir -p examples/asr/timit/data
+
+dataset_name=aishell
+
 python examples/asr/timit/local/prepare_data.py examples/asr/timit/data/TIMIT examples/asr/timit/data
+
+python  athena/cmvn_main.py examples/asr/$dataset_name/configs/mpc.json examples/asr/$dataset_name/data/all.csv
+
+python -m pdb athena/main.py examples/asr/timit/configs/mtl_transformer_sp_101.json
+
+python athena/inference.py examples/asr/timit/configs/mtl_transformer_sp_101.json
+
+
+
